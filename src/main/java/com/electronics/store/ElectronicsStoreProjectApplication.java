f152d9f6514d1f5e04f91811e07f57a1f450da3b
ivikashgorai <vikashgorai985276@gmail.com>
2025-02-26 14:34:12 +0530

    application.properties Setup

---
+++ src/main/java/com/electronics/store/ElectronicsStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.electronics.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectronicsStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicsStoreProjectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/electronics/store/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.electronics.store.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HomeController {
+
+    @GetMapping
+    public String test(){
+        return "Welcome";
+    }
+}
