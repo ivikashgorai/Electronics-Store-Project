e54b1a1989d44a4671fcfbccd075c69f5261b655
ivikashgorai <vikashgorai985276@gmail.com>
2025-02-26 15:06:28 +0530

    User Entity Added

---
+++ src/main/java/com/electronics/store/entities/User.java
@@ -0,0 +1,33 @@
+package com.electronics.store.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+
+//using lombok for getter setter and constructor
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//above is using lombok , in runtime auto. all things will get created
+@Entity
+@Table(name = "users")
+public class User {
+    //can use @Column(name ="") for column name etc.
+     @Id
+    private String userId;
+     @Column(name="user_name",length = 20)
+    private String name;
+    @Column(name="user_email",unique = true)
+    private String email;
+    @Column(name="user_password",length = 10)
+    private String password;
+    private String gender;
+    @Column(length = 1000)
+    private String about;
+    @Column(name="user_image_name") // profile image name
+    private String imageName;
+
+
+}
